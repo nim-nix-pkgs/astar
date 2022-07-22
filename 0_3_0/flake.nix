45d0f005e33d83a1242f49c8b4d280d463f9aa31
hugosenari <hugosenari@gmail.com>
2022-07-21 22:17:29 -0300

    chore: re index astar

---
+++ 0_3_0/flake.nix
@@ -0,0 +1,45 @@
+{
+  description = ''Basic A-Star path finding'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-astar-0_3_0.flake = false;
+  inputs.src-astar-0_3_0.ref   = "refs/tags/0.3.0";
+  inputs.src-astar-0_3_0.owner = "Nycto";
+  inputs.src-astar-0_3_0.repo  = "AStarNim";
+  inputs.src-astar-0_3_0.type  = "github";
+  
+  inputs."binaryheap".owner = "nim-nix-pkgs";
+  inputs."binaryheap".ref   = "master";
+  inputs."binaryheap".repo  = "binaryheap";
+  inputs."binaryheap".dir   = "master";
+  inputs."binaryheap".type  = "github";
+  inputs."binaryheap".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."binaryheap".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."optional_t".owner = "nim-nix-pkgs";
+  inputs."optional_t".ref   = "master";
+  inputs."optional_t".repo  = "optional_t";
+  inputs."optional_t".dir   = "1_2_0";
+  inputs."optional_t".type  = "github";
+  inputs."optional_t".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."optional_t".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-astar-0_3_0"];
+    over = if builtins.pathExists ./override.nix 
+           then { override = import ./override.nix; }
+           else { };
+  in lib.mkRefOutput (over // {
+    inherit self nixpkgs ;
+    src  = deps."src-astar-0_3_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  } );
+}
---
+++ master/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = ''Basic A-Star path finding'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-astar-master.flake = false;
+  inputs.src-astar-master.ref   = "refs/heads/master";
+  inputs.src-astar-master.owner = "Nycto";
+  inputs.src-astar-master.repo  = "AStarNim";
+  inputs.src-astar-master.type  = "github";
+  
+  inputs."binaryheap".owner = "nim-nix-pkgs";
+  inputs."binaryheap".ref   = "master";
+  inputs."binaryheap".repo  = "binaryheap";
+  inputs."binaryheap".dir   = "master";
+  inputs."binaryheap".type  = "github";
+  inputs."binaryheap".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."binaryheap".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-astar-master"];
+    over = if builtins.pathExists ./override.nix 
+           then { override = import ./override.nix; }
+           else { };
+  in lib.mkRefOutput (over // {
+    inherit self nixpkgs ;
+    src  = deps."src-astar-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  } );
+}
